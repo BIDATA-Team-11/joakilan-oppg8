f937910f536d200883e226751fd6db5070c514d5
Joakim Skogø <ajlngvand@gmail.com>
2019-09-27 18:59:16 +0200

    Implementerte påkrevde metoder

--- src/no/iskra/oppg1/Employee.java
+++ src/no/iskra/oppg1/Employee.java
@@ -1,6 +1,7 @@
 package no.iskra.oppg1;
 
 import no.iskra.oppg1.Person;
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 class Employee {
@@ -9,12 +10,42 @@ class Employee {
   float tax;
   final int employeeID;
   final int emplyeeSince;
+  GregorianCalendar calendar;
 
-  Employee(Person person, float salary, float tax, int emplyeeSince, int id) {
-    this.person = person;
+  Employee(String firstName, String lastName, int birthYear, float salary, float tax, int emplyeeSince, int id) {
+    this.person = new Person(firstName, lastName, birthYear);
     this.monthlySalary = salary/12;
     this.tax = tax;
     this.emplyeeSince = emplyeeSince;
     this.employeeID = id;
+    this.calendar = new GregorianCalendar();
+  }
+
+  public float getMonthlyTax() {
+    return (this.tax / 100) * this.monthlySalary;
+  }
+
+  public float getAnnualSalary() {
+    return this.monthlySalary * 12;
+  }
+
+  public float getAnnualTax() {
+    return this.getMonthlyTax() * 10.5;
+  }
+
+  public String getFullName() {
+    return String.format("%s, %s", person.getLastName(), person.getFirstName());
+  }
+
+  public int getAge() {
+    return calendar.get(Calendar.YEAR) - person.getBirthYear();
+  }
+
+  public int getYearsSinceEmployed() {
+    return calendar.get(Calendar.YEAR) - this.emplyeeSince;
+  }
+
+  public bool hasBeenEmployedForXYears(int years) {
+    return this.getYearsSinceEmployed() > years ? true : false;
   }
 }
