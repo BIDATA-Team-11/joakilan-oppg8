4be8fa4e234838d76f373c568061ed415e4ec89d
Joakim Skogø <ajlngvand@gmail.com>
2019-10-04 10:50:01 +0200

    Update test data

--- src/no/iskra/oppg1/TestDataGenerator.java
+++ src/no/iskra/oppg1/TestDataGenerator.java
@@ -5,10 +5,11 @@ import no.iskra.oppg1.Company;
 final class TestDataGenerator {
   public static Company getSampleCompany() {
     Company company = new Company();
-    company.addNewEmployee("Joakim", "Skogø", 1991, 200000f, 33.3f, 2009);
-    company.addNewEmployee("Stine", "Reppe", 1991, 223930.2f, 32.5f, 2014);
-    company.addNewEmployee("Ingvild M.", "Kvisle", 1991, 225400f, 36f, 2013);
-    company.addNewEmployee("Reidar", "Jøssund", 1995, 212500f, 34f, 2012);
+    company.addNewEmployee("Joakim", "Skogø", 1991, 232600f, 33.3f, 2009);
+    company.addNewEmployee("Stine", "Reppe", 1991, 243930.2f, 32.5f, 2014);
+    company.addNewEmployee("Svlad", "Cjelli", 1991, 425400f, 36f, 2013);
+    company.addNewEmployee("Reidar", "Jøssund", 1995, 312500f, 34f, 2012);
+    company.addNewEmployee("Lee", "Scoresby", 1982, 512500f, 34f, 2010);
     return company;
   }
 }
